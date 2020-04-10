187204a47f9b507c2009397c0cf688908c742a64
lbarasti <lbarasti@users.noreply.github.com>
2020-04-10 19:00:59 +0100

    Upgrade to crystal 0.34.0 and update version

--- src/statistics.cr
+++ src/statistics.cr
@@ -5,7 +5,7 @@ require "./lib/distributions"
 # More flexible than a scientific-calculator, but not as exhaustive, yet.
 module Statistics
   extend self
-  VERSION = "0.2.0"
+  VERSION = "0.3.0"
 
   # Computes several descriptive statistics of the passed array.
   #
